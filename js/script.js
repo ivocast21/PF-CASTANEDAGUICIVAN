1e84d9bb30848e38e793ea693f2e46e1e2dd4a5e
Ivan Castañeda <ivancastaneda524@gmail.com>
2023-12-02 15:27:17 -0300

    Arreglamos errores de responsive, y seguimos creando paginas vinculadas

---
+++ js/script.js
@@ -0,0 +1,53 @@
+const items = document.querySelectorAll(".item");
+let imageURLs = [
+    "../media/galeria-media/22.png",
+    "../media/galeria-media/23.png",
+    "../media/galeria-media/24.png",
+    "../media/galeria-media/25.png",
+    "../media/galeria-media/26.png"
+];
+
+let deviceType = "";
+let events = {
+    mouse: {
+        start: "mouseover",
+        end: "mouseout",
+    },
+    touch: {
+        start: "touchstart",
+        end: "touchend",
+    },
+};
+
+const isTouchDevice = () => {
+    try{
+        document.createEvent("TouchEvent");
+        deviceType = "touch";
+        return true;
+    }
+    catch(e){
+        deviceType = "mouse";
+        return false;
+    }
+};
+
+isTouchDevice() ;
+
+items.forEach((item,index) => {
+    let img = document.createElement("img");
+    img.setAttribute("src", imageURLs[index]);
+    img.style.width = "100%";
+    img.style.height = "100%";
+    img.style.objectFit = "cover";
+    item.appendChild(img);
+
+        item.style.flex = "1";
+        item.style.transition = "flex 0.8s ease";
+
+        item.addEventListener(events[deviceType].start, () => {
+            item.style.flex = "4";
+        })
+        item.addEventListener(events[deviceType].end, () => {
+            item.style.flex = "1";
+        })
+});
